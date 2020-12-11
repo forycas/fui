001eab06176d9c58545f1c80be3a33ae835cf191
Mantas Totilas <mantas@totilas.lt>
2020-12-11 22:51:05 +0200

    tailwind.config.js: color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      black: '#000',
+      white: '#fff',
+      emerald: colors.emerald
+    },
     extend: {}
   },
   variants: {
