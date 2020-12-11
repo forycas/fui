e72d969374d6153ab9530b5fee978ceb9468e805
Mantas Totilas <mantas@totilas.lt>
2020-12-11 23:11:15 +0200

    added FTableRow.vue

--- dev/serve.vue
+++ dev/serve.vue
@@ -9,6 +9,33 @@
     <FButton size="large" styling="primary">Link</FButton>
     <FButton styling="transparent">Link</FButton>
 
+    <div class="my-5">
+      <h2 class="text-lg mb-3">Table</h2>
+
+      <FTable>
+        <FTableRow>
+          <FTableCol>Test</FTableCol>
+          <FTableCol>Test col 2</FTableCol>
+          <FTableCol>Test col 3</FTableCol>
+        </FTableRow>
+        <FTableRow odd>
+          <FTableCol>Test</FTableCol>
+          <FTableCol>Test col 2</FTableCol>
+          <FTableCol>Test col 3</FTableCol>
+        </FTableRow>
+        <FTableRow>
+          <FTableCol>Test</FTableCol>
+          <FTableCol>Test col 2</FTableCol>
+          <FTableCol>Test col 3</FTableCol>
+        </FTableRow>
+        <FTableRow :hoverable="false">
+          <FTableCol>Test</FTableCol>
+          <FTableCol>Test col 2</FTableCol>
+          <FTableCol>Test col 3</FTableCol>
+        </FTableRow>
+      </FTable>
+    </div>
+
     <FFormItem label="Test form item" prop="test">
       <FInput />
     </FFormItem>
@@ -26,10 +53,16 @@ import { FButton } from '@/entry.esm'
 import FFormItem from '@/lib-components/FFormItem.vue'
 import FInput from '@/lib-components/FInput.vue'
 import FSelect from '@/lib-components/FSelect.vue'
+import FTable from '@/lib-components/FTable.vue'
+import FTableRow from '@/lib-components/FTableRow.vue'
+import FTableCol from '@/lib-components/FTableCol.vue'
 
 export default defineComponent({
   name: 'ServeDev',
   components: {
+    FTableCol,
+    FTableRow,
+    FTable,
     FSelect,
     FInput,
     FFormItem,
---
+++ src/lib-components/FTableRow.vue
@@ -0,0 +1,18 @@
+<template>
+  <tr class="transition duration-300" :class="[odd ? 'bg-gray-50' : 'bg-white', {'hover:bg-emerald-100 cursor-pointer': hoverable}]">
+    <slot />
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'FTableRow',
+  props: {
+    odd: Boolean,
+    hoverable: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
