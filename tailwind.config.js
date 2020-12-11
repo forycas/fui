b267dbca2d551e64fa0628b65e9bf6a89e6e7c8a
Mantas Totilas <mantas@totilas.lt>
2020-12-11 23:36:34 +0200

    tailwind.config.js: using all colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,7 @@ module.exports = {
   purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      black: '#000',
-      white: '#fff',
-      emerald: colors.emerald
-    },
+    colors,
     extend: {}
   },
   variants: {
