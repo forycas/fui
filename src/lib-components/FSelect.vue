6f97ed08b7e3b31a9827ea4321ab09f0de73c389
Mantas Totilas <mantas@totilas.lt>
2020-12-13 16:39:08 +0200

    FSelect: label styling fix

--- src/lib-components/FSelect.vue
+++ src/lib-components/FSelect.vue
@@ -6,7 +6,7 @@
             aria-expanded="true"
             aria-labelledby="listbox-label"
             class="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-pointer focus:outline-none focus:ring-1 focus:ring-primary-500 focus:border-primary-500 sm:text-sm">
-      <span class="block truncate h-4">
+      <span class="block truncate leading-5 h-5">
         {{ label }}
       </span>
       <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
